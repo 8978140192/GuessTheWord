ebe066307c6b0e11c4910716b3b04c2848d434d0
unknown <Administrator@KANINI-LTP-455.gisbizdev.gisbiz.chennai.com>
2021-09-17 22:52:00 +0530

    'firsts'

---
+++ GuessTheWordApplication/Models/TableUpdation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheWordApplication.Models
+{
+    public class TableUpdation:GuessTheWord
+    {
+        public List<string> FetechAvailableUsers()
+        {
+            List<string> availableUser=new();
+            foreach (var item in context.UserDetails)
+            {
+                availableUser.Add(item.UserName);
+            }
+            return availableUser;
+        }
+    }
+}
